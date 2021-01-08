204c3f56d33cea8709638069c71bca0f99284d95
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2021-01-08 12:07:55 +0900

    feat(viewModel): introduce hilt injection to viewModel

--- app/src/main/java/com/example/myapplication/MyViewModel.kt
+++ app/src/main/java/com/example/myapplication/MyViewModel.kt
@@ -1,9 +1,10 @@
 package com.example.myapplication
 
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MyViewModel : ViewModel() {
+class MyViewModel @ViewModelInject constructor(): ViewModel() {
     var count: Int = 0
     val countLiveData = MutableLiveData<Int>(count)
 
