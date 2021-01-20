b10c5a76d1550f0243ad67ef152ec35debbd6501
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2021-01-20 16:34:47 +0900

    feat(fragment): enable to take photos

--- app/src/main/java/com/example/myapplication/SecondFragment.kt
+++ app/src/main/java/com/example/myapplication/SecondFragment.kt
@@ -1,16 +1,32 @@
 package com.example.myapplication
 
+import android.app.Activity.RESULT_OK
+import android.content.ContentValues
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
+import java.io.FileInputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -31,12 +47,88 @@ class SecondFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
+            // dispatchTakePictureIntent()
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
 
-        view.findViewById<TextView>(R.id.textview_second).text = "Count: " + viewModel.count.toString()
+        view.findViewById<Button>(R.id.button2_2).setOnClickListener {
+            dispatchTakePictureIntent()
+        }
+
+        view.findViewById<TextView>(R.id.textview_second).text =
+            "Count: " + viewModel.count.toString()
         viewModel.countLiveData.observe(viewLifecycleOwner, Observer {
             view.findViewById<TextView>(R.id.textview_second).text = "Count: " + it.toString()
         })
+    }
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    lateinit var currentPhotoPath: String
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = context?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        activity?.packageManager?.also { packageManager ->
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    null
+                }
+                Log.i("Fragment2", "0")
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    Log.i("Fragment2", "1")
+                    Log.i("Fragment2", requireContext().toString())
+                    Log.i("Fragment2", "2")
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        requireContext(),
+                        "com.example.myapplication.fileprovider",
+                        it
+                    )
+
+                    Log.i("Fragment2", "3")
+                    Log.i("Fragment2", photoURI.toString())
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    takePictureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+                    // takePictureIntent.resolveActivity(packageManager)?.also {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                    // }
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val contentValues = ContentValues().apply {
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                put("_data", currentPhotoPath)
+            }
+
+            val inputStream = FileInputStream(File(currentPhotoPath))
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+
+            view?.findViewById<ImageView>(R.id.imageView)?.setImageBitmap(bitmap)
+        }
     }
 }
